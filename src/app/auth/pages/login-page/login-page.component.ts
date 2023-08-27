dc7ac683923f762595d10a88f810b1b5bc67899b
JosephGlz <joseph220199@gmail.com>
2023-08-27 01:40:25 -0600

    style: Stylesheet added

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
 
