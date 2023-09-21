f661ec3b8c9e0b7fbe88e7d87c9efb0caae47b4e
JosephGlz <joseph220199@gmail.com>
2023-09-20 23:21:28 -0600

    feat: middleware on user logged

--- src/app/accounts/pages/profile-page/profile-page.component.ts
+++ src/app/accounts/pages/profile-page/profile-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../../services/account.service';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,11 +9,20 @@ import { AccountService } from '../../services/account.service';
 })
 export class ProfilePageComponent {
   constructor(
-    private _accountService: AccountService
+    private _accountService: AccountService,
+    private _authService: AuthService,
+    private _router: Router
   ) {}
 
 get AccountData() {
   return this._accountService.getAccountData();
+}
+
+ngOnInit(): void {
+  if(this._authService.isSessionExpired()) {
+    this._router.navigate(['/login'])
+    this._authService.logout()
+  }
 }
 
 }
