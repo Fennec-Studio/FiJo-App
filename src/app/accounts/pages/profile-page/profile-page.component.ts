228c0186f921e57d8e77b3ed2c0408d68a0a058f
JosephGlz <joseph220199@gmail.com>
2023-09-26 19:49:20 -0600

    feat: Dialog management

--- src/app/accounts/pages/profile-page/profile-page.component.ts
+++ src/app/accounts/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
 import { AccountService } from '../../services/account.service';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { Router } from '@angular/router';
-
+import { ProfileFormComponent } from '../../components/profile-form/profile-form.component';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html'
@@ -11,7 +14,8 @@ export class ProfilePageComponent {
   constructor(
     private _accountService: AccountService,
     private _authService: AuthService,
-    private _router: Router
+    private _router: Router,
+    private _dialogRef: MatDialog
   ) {}
 
   get AccountData() {
@@ -23,6 +27,21 @@ export class ProfilePageComponent {
       this._router.navigate(['/login'])
       this._authService.logout()
     }
+  }
+
+  openDialog(type: number) {
+    const dialogRef = this._dialogRef.open(ProfileFormComponent, {
+      data: {
+        type
+      },
+      width: '600px',
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        //TODO - Update account data
+      }
+    })
   }
 
 }
