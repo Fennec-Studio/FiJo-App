75059eb6858d36a32dccb13150ee87ca2c02c1c8
Felipe Ramirez <rluisfelipe10@yahoo.com>
2023-09-28 19:49:05 -0600

    feat: add route jobs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
 import { LandingPageComponent } from './landing/pages/landing-page/landing-page.component';
+import { JobsPageComponent } from './jobs/pages/jobs-page/jobs-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: 'jobs', component: JobsPageComponent},
 ];
 
 @NgModule({
