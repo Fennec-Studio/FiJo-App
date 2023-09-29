138d47299a9a600b1fc171c4bf820fdc76c4702f
Felipe Ramirez <rluisfelipe10@yahoo.com>
2023-09-28 19:37:10 -0600

    fix shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent,
+    NavigationBarComponent,
+
   ],
   imports: [
     CommonModule,
     RouterModule,
   ],
   exports: [
-    NavBarComponent
+    NavigationBarComponent
   ],
 })
 export class SharedModule { }
