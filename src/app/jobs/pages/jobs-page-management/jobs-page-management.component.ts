6890f128f3588c5a317aa81cdf3b0af36d6ad481
JosephGlz <joseph220199@gmail.com>
2023-09-30 17:43:42 -0600

    feat: single jobs management page

---
+++ src/app/jobs/pages/jobs-page-management/jobs-page-management.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-page',
+  templateUrl: './jobs-page-management.component.html'
+})
+export class JobsPageManagementComponent {
+
+}
