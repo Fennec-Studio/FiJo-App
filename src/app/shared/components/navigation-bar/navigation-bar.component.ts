bcfb83e4bad6edb66a5b5cd392377c21dc9dfa65
Felipe Ramirez <rluisfelipe10@yahoo.com>
2023-09-30 21:02:37 -0600

    fix: properties & imports

--- src/app/shared/components/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/components/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import { JobsService } from '../../../jobs/services/jobs.service';
+import { AuthService } from '../../../auth/services/auth.service';
+import { AccountService } from 'src/app/accounts/services/account.service';
 
 @Component({
   selector: 'shared-navigation-bar',
@@ -7,17 +10,36 @@ import { fromEvent } from 'rxjs/internal/observable/fromEvent';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent {
-  public sesionActive: boolean = false;
+  public _authService: boolean = false;
   public gender: number = 1;
   public name: string = 'Felipe';
   public lastName: string = 'Ramirez';
 
-  public scroll: boolean = false;
-  constructor() {
+  constructor(private AuthService: AuthService, private _accountService: AccountService) {
+    this._authService = this.AuthService.isSessionExpired();
+
     fromEvent(document, 'scroll').subscribe(
       event => this.scroll = (document.documentElement.scrollTop === 0) ? false : true
     );
   }
+
+  get AccountData() {
+    return this._accountService.getAccountData();
+  }
+
+  getUserLogo() {
+    let logo = '';
+    const name = this.AccountData.firstName.split(' ');
+    const lastName = this.AccountData.lastName.split(' ');
+    logo += name[0].charAt(0);
+    logo += lastName[0].charAt(0);
+
+    return logo.toUpperCase();
+  }
+
+  public scroll: boolean = false;
+
+
 
   // isSesionExpired
 }
