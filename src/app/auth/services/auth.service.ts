f535c0ef5a5989715ad3fc884e4eefd5a8b0fca9
Felipe Ramirez <rluisfelipe10@yahoo.com>
2023-09-30 20:45:07 -0600

    fix: change baseUrl

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = 'http://api.fijo.site';
 
   constructor(
     private _http: HttpClient
