1260b5b70643edc9f606dd5f7a9d7db8b78634a5
JosephGlz <joseph220199@gmail.com>
2023-10-01 02:30:01 -0600

    feat: Job data input added

--- src/app/jobs/components/job-card/job-card.component.ts
+++ src/app/jobs/components/job-card/job-card.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { JobsInfo } from 'src/app/shared/interfaces/JobsInfo';
 
 @Component({
   selector: 'app-job-card',
   templateUrl: './job-card.component.html'
 })
 export class JobCardComponent {
-
+  @Input()
+  jobData: JobsInfo = {} as JobsInfo;
 }
