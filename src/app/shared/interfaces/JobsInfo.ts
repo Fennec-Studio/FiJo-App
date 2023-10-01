d515d7570d648b4bae18b9de6249350e9b82f91e
JosephGlz <joseph220199@gmail.com>
2023-10-01 00:40:08 -0600

    feat: Jobs data interface

---
+++ src/app/shared/interfaces/JobsInfo.ts
@@ -0,0 +1,16 @@
+export interface JobsInfo {
+  JID: number;
+  JOBTITLE: string;
+  JOBDESCRIPTION: string;
+  LOCATION: string;
+  REQUERIMENTS: string;
+  BENEFITS: string;
+  STARTDATE: string;
+  EMPLOYMENTTYPE: string;
+  SALARY: string;
+  APPLICATION: string;
+  CONTACT: string;
+  STATUS: number;
+  EID_FK: number;
+  CREATION_DATE: string;
+}
