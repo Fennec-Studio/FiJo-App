50f27fc19a532328f0d4ff4db02ae381adbcda10
JosephGlz <joseph220199@gmail.com>
2023-10-01 00:41:54 -0600

    feat: Components and modules imported

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 import { JobsPageManagementComponent } from './pages/jobs-page-management/jobs-page-management.component';
 import { SharedModule } from '../shared/shared.module';
 import { SubmenuComponent } from './components/submenu/submenu.component';
+import { JoblistComponent } from './components/joblist/joblist.component';
+import { ApplicantlistComponent } from './components/applicantlist/applicantlist.component';
+import { JobsconfigComponent } from './components/jobsconfig/jobsconfig.component';
+import { HttpClientModule } from '@angular/common/http';
+import { JobCardComponent } from './components/job-card/job-card.component';
 
 @NgModule({
   declarations: [
     JobsPageManagementComponent,
-    SubmenuComponent
+    SubmenuComponent,
+    JoblistComponent,
+    ApplicantlistComponent,
+    JobsconfigComponent,
+    JobCardComponent
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
+
     SharedModule,
   ],
   exports: [
