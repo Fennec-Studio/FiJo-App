ed3fe037c41cf6c068bbb7dfacbc63c06a35fe9d
JosephGlz <joseph220199@gmail.com>
2023-10-01 00:40:32 -0600

    feat: data pass trough event emitter

--- src/app/jobs/components/submenu/submenu.component.ts
+++ src/app/jobs/components/submenu/submenu.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-jobs-submenu',
   templateUrl: './submenu.component.html'
 })
 export class SubmenuComponent {
-  public index = 0;
+  idxSubmenu: number = 0;
+  @Output()
+  public idx: EventEmitter<number> = new EventEmitter<number>();
 
   changeIndex(index: number) {
-    this.index = index;
+    this.idxSubmenu = index;
+    this.idx.emit(this.idxSubmenu);
   }
 }
--- src/app/jobs/pages/jobs-page-management/jobs-page-management.component.ts
+++ src/app/jobs/pages/jobs-page-management/jobs-page-management.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-jobs-page',
   templateUrl: './jobs-page-management.component.html'
 })
 export class JobsPageManagementComponent {
+  @Input() idx: number = 0;
 
+  changeIndex(index:any) {
+    this.idx = index;
+  }
 }
