cdddc447c75343293cb71eb102d7290648d2c4fe
JosephGlz <joseph220199@gmail.com>
2023-10-01 00:41:42 -0600

    feat: Jobs views added

---
+++ src/app/jobs/components/job-card/job-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html'
+})
+export class JobCardComponent {
+
+}
---
+++ src/app/jobs/components/jobsconfig/jobsconfig.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-config',
+  templateUrl: './jobsconfig.component.html'
+})
+export class JobsconfigComponent {
+
+}
