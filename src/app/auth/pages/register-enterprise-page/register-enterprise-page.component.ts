3aebe4963a04874f25e39ec742c44d1eb6f3ca4d
Felipe Ramirez <rluisfelipe10@yahoo.com>
2023-10-02 02:05:49 -0600

    feature: add register as enterprise first data

---
+++ src/app/auth/pages/register-enterprise-page/register-enterprise-page.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-register-enterprise-page',
+  templateUrl: './register-enterprise-page.component.html',
+  styleUrls: ['./register-enterprise-page.component.css']
+})
+export class RegisterEnterprisePageComponent {
+
+  public registerFormModel = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  public showPassword = false
+  public showconfirmPassword = false
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private _router: Router,
+    private _authService: AuthService
+  ) { }
+
+
+  ngOnInit(): void {
+    if(!this._authService.isSessionExpired()) {
+      this._router.navigate(['/'])
+    }
+  }
+
+  public onRegisterSubmit(): void {
+    if(this.validateData(this.registerFormModel)) {
+      console.log('registerFormModel', this.registerFormModel);
+      this._router.navigate(['/register-enterprise/data'], { state: { data: this.registerFormModel } })
+    }
+  }
+
+  validateData(data: any): boolean {
+    const emailRegex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')
+    const namesRegex = new RegExp('^[a-zA-ZÀ-ÿ\u00f1\u00d1 ]+$')
+
+    if(!data.email || !data.password || !data.confirmPassword || !data.name) {
+      this.openSnackBar('Todos los campos son requeridos')
+      return false
+    } else {
+      if(data.password !== data.confirmPassword) {
+        this.openSnackBar('Las contraseñas no coinciden')
+        return false
+      } else {
+        if(!emailRegex.test(data.email)) {
+          this.openSnackBar('El correo es inválido')
+          return false
+        } else {
+          if(!namesRegex.test(data.name)) {
+            this.openSnackBar('El nombre solo puede contener letras')
+            return false
+          }
+        }
+      }
+    }
+
+    return true
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, 'Aceptar', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      duration: 5000,
+    })
+  }
+
+  public togglePasswordVisibility(value: number): void {
+    if(value === 1) {
+      this.showPassword = !this.showPassword
+    } else {
+      this.showconfirmPassword = !this.showconfirmPassword
+    }
+  }
+}
