a81fdfdaa853e0ec19129aed357c7da017f9451e
JosephGlz <joseph220199@gmail.com>
2023-10-07 14:51:48 -0600

    feat: api implemented

--- src/app/accounts/services/account.service.ts
+++ src/app/accounts/services/account.service.ts
@@ -1,11 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EnterpriseAccount } from 'src/app/shared/interfaces/EnterpriseAccount';
 import { UserAccount } from 'src/app/shared/interfaces/UserAccount';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+
+  private urlBase = `http://localhost:3000/`
+  // private urlBase = `http://api.fijo.site`
+
   public statesList: string[] = [
     'Aguascalientes', 'Baja California', 'Baja California Sur',
     'Campeche', 'Chiapas', 'Chihuahua',
@@ -20,7 +25,6 @@ export class AccountService {
     'Yucatán', 'Zacatecas'
   ]
 
-
   constructor(
     private _http: HttpClient,
   ) { }
@@ -29,9 +33,23 @@ export class AccountService {
     return this._http.post('https://countriesnow.space/api/v0.1/countries/state/cities', { "country": "Mexico", "state": state });
   }
 
+  getProfileData(id: number) {
+    return this._http.get(`${this.urlBase}user/profile?id=${id}`);
+  }
+
   getAccountData(): any  {
       const user = localStorage.getItem('user');
+      const uuid = JSON.parse(user!).uuid;
       return JSON.parse(user!);
+  }
 
+  getAccountUUID(): number {
+    const user = localStorage.getItem('user');
+    const uuid = JSON.parse(user!).uuid;
+    return uuid;
+  }
+
+  public getBusinessData(): EnterpriseAccount {
+    return JSON.parse(localStorage.getItem('user')!);
   }
 }
